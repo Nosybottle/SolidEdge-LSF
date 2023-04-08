352e8ce147d10e1389fc27f4c774164954657dfb
Nosybottle <nosybottle@gmail.com>
2023-04-08 17:49:40 +0200

    Fix plane asking for 6 points

--- gui/mainapplication.py
+++ gui/mainapplication.py
@@ -119,9 +119,10 @@ class MainApplication(ttk.Frame):
     def fit_plane(self):
         """Fit plane through the selected points"""
         points = self.vertex_selector.get_coordinates()
+        print(f"{points = }")
         if points is None:
             return
-        if len(points) < 6:
+        if len(points) < 3:
             logger.error(lang.errors.plane_points)
             return
 
