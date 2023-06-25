a65d94a166879edb08e5ffd9f552cf4f47d41687
Nosybottle <nosybottle@gmail.com>
2023-06-25 17:17:44 +0200

    Check specified language file exists. If not, throw an error

--- config/__init__.py
+++ config/__init__.py
@@ -1,13 +1,25 @@
+import os
+import logging
+
 from config.config import _Config
+
+logger = logging.getLogger("LSF")
 
 _config_file = "config/settings.ini"
 _language_folder = "config/lang"
 
 
-def load_config() -> None:
+def load_config() -> bool:
     """Load configuration and language data"""
     config.load_config(_config_file)
-    lang.load_config(f"{_language_folder}/{config.language}.ini")
+
+    language_file = f"{_language_folder}/{config.language}.ini"
+    if not os.path.isfile(language_file):
+        logger.error(f"Incorrect configuration|Selected language (\"{config.language}\") doesn't exist")
+        return False
+
+    lang.load_config(language_file)
+    return True
 
 
 config = _Config()
